6e38e67180226c02783eeaf912a6375d397e0f3d
papudas2004 <pdas91450@gmail.com>
2026-07-07 22:27:14 +0530

    Fix CSS filename case

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
